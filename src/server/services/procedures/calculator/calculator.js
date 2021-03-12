4227973c45ac0bba2af79d35b8de58f202c60ed5
SahasGoli <sahas.goli@vanderbilt.edu>
2021-03-11 20:23:35 -0600

    Changed to a list-based problem method. Added unit tests for the four basic operations.

--- src/server/services/procedures/calculator/calculator.js
+++ src/server/services/procedures/calculator/calculator.js
@@ -7,37 +7,65 @@ const CalculatorService = {};
 
 /* TODO
 In the future it would be great to have a way to define how challenges are graded -- a selection of grading schemes
---> Ex: Series of checkpoints/unit tests (Correctly does addition, mult, etc...)
-Add unit testing for add/sub/mult/divide to see which they pass
-NEXT STEPS:
--> Add scoring per user (challenge/response)  with unit testing or scaling difficulty  
--> OR could make student process a list of problems at once and just return a list of which they got correct 
 */
 let answerStorage = new Map(); 
 /**
  * Test whether the users answer to the math problem was correct
  * 
- * @param {Number} userAnswer Users answer to the math problem.
- * @returns {String} Correctness of answer.
+ * @param {Array} userAnswers Users answers to the math problems.
+ * @returns {String} A string representing how many answers were correct.
  */
-CalculatorService.checkAnswer = function(userAnswer) {
+CalculatorService.checkAnswers = function(userAnswers) {
 	const username = this.caller.username;
-    return userAnswer == answerStorage.get(username) ? "Correct" : "Incorrect";
+	var solutions = answerStorage.get(username);
+	let num = solutions.length;
+	var correct = 0;
+	for(var i = 0; i < userAnswers.length; i++) {
+		if(userAnswers[i] == solutions[i]) correct++;
+	}
+    return "You got " + correct + " / " + num + " problems correct.";
 };
 
 /**
- * Create a random math problem for the user
- * @returns {String} Math problem
+ * Create a list of random math problems for the user
+ * Expects the user to have set up a system to accept problems as a list, solve them/store them in an answers array, and then call the checkAnswer block.
+ * @returns {Array} An array of random math problems.
  */
-CalculatorService.requestRandomProblem = function() {
+CalculatorService.requestRandomProblems = function() {
 	const username = this.caller.username;
+	var problems = [];
+	var answers = [];
 	const ops = ['+', '-', '*', '/'];
-	const chosenOp = ops[Math.floor(Math.random()*ops.length)];
-	var first = Math.ceil(Math.random() * 9999) * (Math.round(Math.random()) ? 1 : -1);
-	var second = Math.ceil(Math.random() * 9999) * (Math.round(Math.random()) ? 1 : -1);
-	var problem = "(" + first + ") " + chosenOp + " (" + second + ")";
-	answerStorage.set(username, eval(problem));
-    return problem;
+	for(var i = 0; i < 20; i++) {
+		const chosenOp = ops[Math.floor(Math.random()*ops.length)];
+		var first = Math.ceil(Math.random() * 999) * (Math.round(Math.random()) ? 1 : -1);
+		var second = Math.ceil(Math.random() * 999) * (Math.round(Math.random()) ? 1 : -1);
+		problems.push("(" + first + ") " + chosenOp + " (" + second + ")");
+		answers.push(eval(problems[i]));
+	}
+	answerStorage.set(username, answers);
+    return problems;
 };
+
+/**
+ * Performs tests on the four basic operators. 
+ * Expects the user to have set up a system to accept problems as a list, solve them/store them in an answers array, and then call the checkAnswer block.
+ * @returns {Array} An array of math problems to test the four basic arithmetic operations (+, -, /, *)
+ */
+CalculatorService.basicOpsTest = function() {
+	const username = this.caller.username;
+	var problems = [];
+	var answers = [];
+	for(var i = 0; i < ops.length; ++i) {
+		const ops = ['+', '-', '*', '/'];
+		const chosenOp = ops[i];
+		var first = Math.ceil(Math.random() * 99) * (Math.round(Math.random()) ? 1 : -1);
+		var second = Math.ceil(Math.random() * 99) * (Math.round(Math.random()) ? 1 : -1);
+		problems.push("(" + first + ") " + chosenOp + " (" + second + ")");
+		answers.push(eval(problems[i]));
+	}
+	answerStorage.set(username, answers);
+	return problems;
+}
 
 module.exports = CalculatorService;
